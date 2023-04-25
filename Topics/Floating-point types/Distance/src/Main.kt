b0c3ae75c6dfac1815625a700da2592b5daab4b8
adistroe <97851663+adistroe@users.noreply.github.com>
2023-04-25 20:06:28 +0300

    Delete Topics directory

--- Topics/Floating-point types/Distance/src/Main.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    // put your code here
-    val (distance, time) = List(2) { readln().toInt() }
-    print(distance / time)
-}
--- Topics/What is an exception/Cause an exception/src/Main.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    // Write some code to cause an exception
-    var a = readln().toInt()
-    val b = a++
-}
